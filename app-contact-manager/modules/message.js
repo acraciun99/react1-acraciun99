a89961f41bdf151f3f716bccd8006bce1a15f328
acraciun99 <87021081+acraciun99@users.noreply.github.com>
2022-09-16 10:34:44 +0300

    message close button and refactor

--- app-contact-manager/modules/message.js
+++ app-contact-manager/modules/message.js
@@ -1,9 +1,25 @@
-export default (message = '', type = 'primary') => {
-  const messageContainer = document.createElement('div');
-  messageContainer.classList.add('alert', `alert-${type}`);
+export default (message = '', type = 'primary', element = 'div') => {
+  const messageContainer = document.createElement(element);
+  messageContainer.classList.add(
+    'alert',
+    `alert-${type}`,
+    `d-flex`,
+    `justify-content-between`,
+    `align-items-center`,
+  );
 
-  // FTI esista un textContent
+  // FTI exista un textContent
   messageContainer.textContent = message;
+
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-danger');
+  button.textContent = 'Close';
+
+  messageContainer.append(button);
+
+  button.addEventListener('click', () => {
+    messageContainer.remove();
+  });
 
   return messageContainer;
 };
